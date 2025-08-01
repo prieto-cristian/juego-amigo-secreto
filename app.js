03a9778571103d7899f9f114b61d21f08a84ba79
Prieto Cristian <pcristiandev@gmail.com>
2025-08-01 11:52:40 -0300

    Implemente la funciondad para agregar amigos.
    Ahora se pueden agregar amigos en la pagina web.
    Se valida que el usuario no intente cargar el
    campo estando vacio. En caso de estar vacio no se
    agrega a la lista y se cambia el mensaje del
    titulo para que el usuario se de cuenta. Tambien
    se cambia el mensaje del titulo cada vez que el
    usuario agregar un nombre correctamente.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    // Capturar el valor ingresado en el campo de texto
+    // Aniadir a la lista
+    let campoTexto = document.getElementById('amigo');
+    let titulo = document.querySelector('.section-title');
+    if(esCampoVacio(campoTexto.value)){
+        titulo.innerHTML = "Por favor, inserte un nombre.";
+    }else{
+        amigos.push(campoTexto.value);
+        titulo.innerHTML = "Agregado correctamente";
+        campoTexto.value = "";
+    }
+    console.log(amigos);
+}
+
+function esCampoVacio(textoIngresado) {
+    if(textoIngresado === ''){
+        return true;
+    }
+    return false
+}
